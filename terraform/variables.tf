64ce2a2a9b283bc6b9c88281596c921e7e42c045
Chinmaya Prasad Sahoo <chinmayash11@gmail.com>
2026-05-19 08:53:48 +0530

    initial commit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,11 +4,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "aws_role_arn" {
-  description = "ARN of the IAM role for GitHub Actions to assume (created for OIDC)"
-  type        = string
-}
-
 variable "ssh_key_name" {
   description = "SSH key pair name attached to EC2 instances"
   type        = string
